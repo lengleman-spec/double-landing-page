f7f354acd8eb7ce415a382c8bd4459c605ef9b8b
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2026-03-17 13:34:39 -0400

    Add event listener for mouseover and mouseleave of left element.

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ const containerEl = document.querySelector(".container");
 
 const leftEl = document.querySelector(".left");
 const rightEl = document.querySelector(".right");
+
+leftEl.addEventListener("mouseover", () => {
+  // add class method
+  containerEl.classList.add("active-left");
+});
+
+leftEl.addEventListener("mouseleave", () => {
+  containerEl.classList.remove("active-left");
+});
